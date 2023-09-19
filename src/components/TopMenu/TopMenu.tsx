e853928f5e838f5db00e4d3ffe5ecd03ec438244
illia_kots <illyahennadievich@gmail.com>
2023-09-19 14:20:49 +0300

    Fixes .jsons and image location

--- src/components/TopMenu/TopMenu.tsx
+++ src/components/TopMenu/TopMenu.tsx
@@ -4,18 +4,19 @@ import './TopMenu.scss';
 export const TopMenu: React.FC = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
 
-  const optionsDate = {
+  const optionsDate: Intl.DateTimeFormatOptions = {
     day: '2-digit',
     month: 'short',
     year: 'numeric',
   };
 
-  const optionsTime = {
+  const optionsTime: Intl.DateTimeFormatOptions = {
     hour: '2-digit',
     minute: '2-digit',
   };
 
-  const optionsToday = { weekday: 'long' };
+  const optionsToday: Intl.DateTimeFormatOptions = { weekday: 'long' };
+
   const today = currentDate.toLocaleString(undefined, optionsToday);
   const time = currentDate.toLocaleString(undefined, optionsTime);
   const date = currentDate.toLocaleString(undefined, optionsDate).toUpperCase();
